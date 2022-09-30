698b26b468cf391e9fa0bc367665ca454e6c8d84
Chema Lopez <jlopezpu@nttdata.com>
2022-09-30 16:24:30 +0200

    Added Component department

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { DepartmentComponent } from './Components/department/department.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    DepartmentComponent,
     
   ],
   imports: [
